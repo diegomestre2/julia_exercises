93977d7352916436fddaf8925c04effa4a038bf3
Diego Gomes Tome <diegogomest@gmail.com>
2021-07-09 12:28:39 +0200

    Files chap2 abstract types

---
+++ src/asset.jl
@@ -0,0 +1,20 @@
+abstract type Asset end
+
+abstract type Property <: Asset end
+abstract type Investment <: Asset end
+abstract type Cash <: Asset end
+
+abstract type House <: Property end
+abstract type Apartment <: Property end
+
+abstract type FixedIncome <: Investment end
+abstract type Equity <: Investment end
+
+# Display the entire type hierarch starting from the specified 'roottype'
+function subtypetree(roottype, level=1, indent=4)
+    level == 1 && println(roottype)
+    for s in subtypes(roottype)
+        println(join(fill(" ", level * indent)) * string(s))
+        subtypetree(s, level + 1, indent)
+    end
+end
---
+++ src/main.jl
@@ -0,0 +1,7 @@
+include("Calculator.jl")
+include("asset.jl")
+
+x = interest(10, 0.1)
+println("Interest: ", x)
+sum_res = sum(x, 10)
+println("Sum: ", sum_res)
