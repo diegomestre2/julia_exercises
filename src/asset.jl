1dd37d62dd8c095912764cf444dec3ed7c959970
Diego Gomes Tome <diegogomest@gmail.com>
2021-07-09 13:16:56 +0200

    More exercises chap2 abstract types

--- src/asset.jl
+++ src/asset.jl
@@ -18,3 +18,34 @@ function subtypetree(roottype, level=1, indent=4)
         subtypetree(s, level + 1, indent)
     end
 end
+
+# simple functions on abstract types
+describe(a::Asset) = "Something valuable"
+describe(e::Investment) = "Financial investment"
+describe(e::Property) = "Physical property"
+
+"""
+    location(p::Property)
+Returns the location of the property as a tuple of (latitude, longitude).
+"""
+location(p::Property) = error("Location is not defined int he concrete type")
+
+function walking_distance(p1::Property, p2::Property)
+    loc1 = location(p1)
+    loc2 = location(p2)
+    return abs(loc1.x - loc2.x) + abs(loc1.y - loc2.y)
+end
+
+mutable struct Stock <: Equity
+    symbol::String
+    name::String
+end
+
+function describe(s::Stock)
+    return s.symbol * "(" * s.name * ")"
+end
+
+struct BasketOfStocks
+    stocks::Vector{Stock}
+    reason::String
+end
--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,4 @@
+
 include("Calculator.jl")
 include("asset.jl")
 
