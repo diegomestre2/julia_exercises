0c859c01900e832118b8c1956e4619d737aab186
Diego Gomes Tome <diegogomest@gmail.com>
2021-07-15 10:57:20 +0200

    More Chap3 code

--- src/Chapter3/widgets.jl
+++ src/Chapter3/widgets.jl
@@ -30,6 +30,11 @@ Base.show(io::IO, p::Position) = print(io, "(", p.x, ",", p.y, ")")
 Base.show(io::IO, s::Size) = print(io, s.width, " x ", s.height)
 Base.show(io::IO, w::Widget) = print(io, w.name, " at ", w.position, " size ", w.size)
 
+function make_asteroids(N::Int, pos_range=0:200, size_range=10:30)
+    pos_rand() = rand(pos_range)
+    sz_rand() = rand(size_range)
+    return [Widget("Asteroid #$i", Position(pos_rand(), pos_rand()), Size(sz_rand(), sz_rand())) for i in 1:N]
+end
 
 function main()
     w = Widget("asteroid", Position(0, 0), Size(10, 20))
@@ -37,6 +42,7 @@ function main()
     move_down!(w, 10)
     move_left!(w, 20)
     move_right!(w, 20)
+    
     # it should be back to Position(0, 0)
     print(w)
 end
